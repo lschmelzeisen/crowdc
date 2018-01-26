066affd9aeb24b1da6396926f0031f7966f8c601
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2018-01-26 20:16:29 +0100

    Require Phaser and deps.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello from index.js!')
+window.PIXI = require('phaser/build/custom/pixi')
+window.p2 = require('phaser/build/custom/p2')
+window.Phaser = require('phaser/build/custom/phaser-split')
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
+
+function preload() {
+    game.load.image('arrow', 'assets/sprites/arrow.png');
+}
+
+var sprite;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#0072bc';
+
+    sprite = game.add.sprite(400, 300, 'arrow');
+    sprite.anchor.setTo(0.5, 0.5);
+
+    //  Enable Arcade Physics for the sprite
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+    //  Tell it we don't want physics to manage the rotation
+    sprite.body.allowRotation = false;
+
+}
+
+function update() {
+
+    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
+
+}
+
+function render() {
+
+    game.debug.spriteInfo(sprite, 32, 32);
+
+}
