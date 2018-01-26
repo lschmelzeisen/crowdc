7537c81c1c20577a55884ecf224f71311719145e
Carsten König <carstenkoenig92@gmail.com>
2018-01-27 00:28:53 +0100

    - cleanup

--- src/modules/human.js
+++ src/modules/human.js
@@ -6,16 +6,16 @@ export default class Human {
 
         this.sprite = game.add.sprite(this.origin.x, this.origin.y, 'trap');
         this.sprite.scale.setTo(0.5, 0.5)
+        game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 
         this.moveToTarget(game)
     }
 
     moveToTarget(game) {
         this.target = getRandomGridPoint(game.width, game.height);
-        game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 
         let speed = getRandomArbitrary(500, 5000); // random between 0.5s and 5s
-        game.physics.arcade.moveToXY(this.sprite, this.target.x, this.target.y, 60, speed)
+        game.physics.arcade.moveToXY(this.sprite, this.target.x, this.target.y, 0, speed)
 
         if (this.timer)
             clearTimeout(this.timer);
