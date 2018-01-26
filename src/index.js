f3d32af2ec9b3872c89fb39aea7a555e95a626b4
Carsten König <carstenkoenig92@gmail.com>
2018-01-26 22:41:57 +0100

    - move example code to class

--- src/index.js
+++ src/index.js
@@ -5,44 +5,42 @@ require('phaser');
 import Arrow from './assets/sprites/arrow.png';
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
-    preload: preload,
-    create: create,
-    update: update,
-    render: render
-});
+class Crowdc {
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
+            preload: this.preload,
+            create: this.create,
+            update: this.update,
+            render: this.render
+        })
+    }
 
-function preload() {
-    game.load.image('arrow', Arrow);
+    preload() {
+        this.game.load.image('arrow', Arrow);
+    }
+
+    create() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.game.stage.backgroundColor = '#0072bc';
+
+        this.sprite = this.game.add.sprite(400, 300, 'arrow');
+        this.sprite.anchor.setTo(0.5, 0.5);
+
+        //  Enable Arcade Physics for the sprite
+        this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+
+        //  Tell it we don't want physics to manage the rotation
+        this.sprite.body.allowRotation = false;
+    }
+
+    update() {
+        this.sprite.rotation = this.game.physics.arcade.moveToPointer(this.sprite, 60, this.game.input.activePointer, 500);
+    }
+
+    render() {
+        this.game.debug.spriteInfo(this.sprite, 32, 32);
+    }
 }
 
-var sprite;
-
-function create() {
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    game.stage.backgroundColor = '#0072bc';
-
-    sprite = game.add.sprite(400, 300, 'arrow');
-    sprite.anchor.setTo(0.5, 0.5);
-
-    //  Enable Arcade Physics for the sprite
-    game.physics.enable(sprite, Phaser.Physics.ARCADE);
-
-    //  Tell it we don't want physics to manage the rotation
-    sprite.body.allowRotation = false;
-
-}
-
-function update() {
-
-    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
-
-}
-
-function render() {
-
-    game.debug.spriteInfo(sprite, 32, 32);
-
-}
+var game = new Crowdc();
