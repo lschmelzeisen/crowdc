0d353a1199036bfa6a22fc2a5e9e9e1c93141f71
Carsten König <carstenkoenig92@gmail.com>
2018-01-26 20:26:56 +0100

    - load phaser via npm and webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
 
+var phaserModulePath = path.join(__dirname, '../node_modules/phaser/');
+
 module.exports = {
     entry: './src/index.js',
     devServer: {
         contentBase: './dist'
-    },
-    output: {
+    }, module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {test: /pixi\.js/, loader: 'expose-loader?PIXI'},
+            {test: /phaser-split\.js$/, loader: 'expose-loader?Phaser'},
+            {test: /p2\.js/, loader: 'expose-loader?p2'},]
+    }, output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            'phaser': path.join(phaserModulePath, 'build/custom/phaser-split.js'),
+            'pixi': path.join(phaserModulePath, 'build/custom/pixi.js'),
+            'p2': path.join(phaserModulePath, 'build/custom/p2.js')
+
+        }
     }
 };
 
