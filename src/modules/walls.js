8e7eaa04a147e819bddb673113d39cadc8c77402
Carsten König <carstenkoenig92@gmail.com>
2018-01-27 23:09:22 +0100

    - fix unmerged merge conflict

--- src/modules/walls.js
+++ src/modules/walls.js
@@ -12,11 +12,8 @@ export default class Walls {
             this.state.game.input.activePointer.worldX,
             this.state.game.input.activePointer.worldY));
         pos = this.state.grid.toGridCoords(pos.x, pos.y);
-<<<<<<< HEAD
         if (pos.x >= 0 && pos.x < this.state.map.width && pos.y >= 0 && pos.y < this.state.map.height)
-            this.walls.create(pos.x,pos.y,'wall',0);
-=======
-        this.walls.create(pos.x, pos.y, 'wall', 0);
->>>>>>> 7df8a6ba8494495274ed9a8ebe0c01acbd400896
+            this.walls.create(pos.x, pos.y, 'wall', 0);
+
     }
 }
