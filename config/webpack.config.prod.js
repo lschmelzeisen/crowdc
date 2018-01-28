3a71a003ca6fb0b055d756e898b5263d1edfadce
Carsten König <carstenkoenig92@gmail.com>
2018-01-28 15:12:56 +0100

    hash bundle file in webpack prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,7 +14,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js',
     },
     plugins: [
         new HtmlWebpackPlugin({
