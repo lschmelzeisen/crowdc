711f68bab7f8678c706276436019ec65bfecc7b3
Carsten König <carstenkoenig92@gmail.com>
2018-01-28 12:40:38 +0100

    fix cache bug with dead blue guys

--- src/modules/human.js
+++ src/modules/human.js
@@ -47,6 +47,8 @@ export class Human {
     }
 
     destroy() {
+        if (this.makeSickTimer)
+            clearTimeout(this.makeSickTimer);
         this.state.removeSprite(this);
     }
 
@@ -80,7 +82,7 @@ export class Human {
         this.state.healthyGroup.remove(this.sprite);
         this.sprite.loadTexture('orb-blue');
         this.state.infectedGroup.add(this.sprite);
-        setTimeout(() => this.makeSick(), 5000)
+        this.makeSickTimer = setTimeout(() => this.makeSick(), 5000)
     }
 
     makeSick() {
